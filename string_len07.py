b85d1a995195305843ae3e2a853586f57b371083
unknown <princariens@gmail.com>
2022-03-27 07:38:34 +0500

    07 ch

--- string_len07.py
+++ string_len07.py
@@ -9,11 +9,11 @@ def main(s1,s2,s3):
         string
     """
     a=''
-    if len(s1)%2==0:
+    if len(s1)%2==1:
         a+=s1+', '
-    if len(s2)%2==0:
+    if len(s2)%2==1:
         a+=s2+', '
-    if len(s3)%2==0:
+    if len(s3)%2==1:
         a+=s3+', '
     if a=='':
         return "[]"
