5953a5fae4fe30152386a8789105a03a4bdd5316
unknown <princariens@gmail.com>
2022-03-27 17:18:39 +0500

    00

--- string_len07.py
+++ string_len07.py
@@ -10,12 +10,12 @@ def main(s1,s2,s3):
     """
     a=''
     if len(s1)%2==1:
-        a+=s1+', '
+        a=a+s1+','
     if len(s2)%2==1:
-        a+=s2+', '
+        a=a+s2+','
     if len(s3)%2==1:
-        a+=s3+', '
+        a=a+s3+','
     if a=='':
         return "[]"
     else:
-        return str('['+a+'\b\b]')
+        return "["+a+"\b]"
